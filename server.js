4d477113bf00f9d316d046b714076cb562c349db
riyavmt <riyavmt14@gmail.com>
2023-11-14 16:43:29 +0530

    Initial commit

--- server.js
+++ server.js
@@ -5,11 +5,15 @@ const app = express();
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const contactRoutes = require('./routes/contact');
+const successRoutes = require('./routes/success');
 const path = require('path');
 app.use(bodyParser.urlencoded({extended:false}));
 
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
+app.use(contactRoutes);
+app.use(successRoutes);
 app.use((req,res,next)=>{
     res.status(404).sendFile(path.join(__dirname,'views','404.html'));
 })
