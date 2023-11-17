dc377c3e5aa413c2f29b376210d5098920e3636c
riyavmt <riyavmt14@gmail.com>
2023-11-17 13:54:21 +0530

    Added Details Page

--- controllers/404.js
+++
@@ -1,3 +0,0 @@
-exports.get404Page = (req,res)=>{
-    res.status(404).send('<h1>Error 404 not found</h1>');
-};
--- routes/contact-us.js
+++
@@ -1,9 +0,0 @@
-const express =require('express');
-const router = express.Router();
-const path = require('path');
-const rootDir = require('../util/path');
-const productsController = require('../controllers/products');
-
-router.get('/contact-us',productsController.getContact);
-router.post('/contact-us',productsController.postContact );
-module.exports = router;
